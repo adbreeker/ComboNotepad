a0fee3cc5891ac6e8b3e02574e32540b5acf3db3
adbreeker <111668308+adbreeker@users.noreply.github.com>
2022-12-14 01:39:49 +0100

    Add files via upload

--- src/main/java/com/example/fingerprintnotepad/MainActivity.kt
+++ src/main/java/com/example/fingerprintnotepad/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fingerprintnotepad
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -20,70 +21,32 @@ import java.util.concurrent.Executor
 class MainActivity : AppCompatActivity()
 {
     private var isAuthenticated : Boolean = false
-    private lateinit var executor: Executor
-    private lateinit var biometricPrompt: BiometricPrompt
-    private lateinit var promptInfo: PromptInfo
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        isAuthenticated = intent.getBooleanExtra("isAuthenticated", false)
 
-        checkDeviceBiometric()
-        setupBiometric()
-
-        biometricPrompt.authenticate(promptInfo)
+        if(isAuthenticated)
+        {
+            loadData()
+        }
 
 
         SaveButton.setOnClickListener()
         {
             saveData()
         }
-    }
 
-    private fun checkDeviceBiometric()
-    {
-        val biometricManager = BiometricManager.from(this)
-        when(biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL))
+        ChangePasswordButton.setOnClickListener()
         {
-            BiometricManager.BIOMETRIC_SUCCESS -> {
-                Toast.makeText(this, "Can authenticate", Toast.LENGTH_LONG).show()
-            }
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->{
-                Toast.makeText(this, "Can not authenticate", Toast.LENGTH_LONG).show()
-            }
+            saveData()
+            startActivity(Intent(this, ChangePasswordActivity::class.java))
         }
     }
 
 
-    private fun setupBiometric()
-    {
-        executor = ContextCompat.getMainExecutor(this)
-        biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback(){
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                super.onAuthenticationError(errorCode, errString)
-                Toast.makeText(this@MainActivity, "authentication error: $errString", Toast.LENGTH_LONG).show()
-            }
-
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                super.onAuthenticationSucceeded(result)
-                Toast.makeText(this@MainActivity, "authenticated, permission granted", Toast.LENGTH_SHORT).show()
-                isAuthenticated = true
-                loadData()
-            }
-
-            override fun onAuthenticationFailed() {
-                super.onAuthenticationFailed()
-                Toast.makeText(this@MainActivity, "authentication failed", Toast.LENGTH_SHORT).show()
-            }
-        })
-
-        promptInfo = PromptInfo.Builder()
-            .setTitle("Fingerprint Authentication")
-            .setSubtitle("Authenticate to get read/write permission")
-            .setNegativeButtonText("Cancel")
-            .build()
-    }
 
 
 
@@ -98,7 +61,7 @@ class MainActivity : AppCompatActivity()
             {
                 file.createNewFile()
             }
-            var fos = FileOutputStream(file)
+            val fos = FileOutputStream(file)
             cryptoManager.encrypt(bytes = bytesToEncrypt, outputStream = fos)
 
             Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show()
@@ -121,7 +84,7 @@ class MainActivity : AppCompatActivity()
         }
         else
         {
-            var decrypted = cryptoManager.decrypt(
+            val decrypted = cryptoManager.decrypt(
                 inputStream = FileInputStream(file)
             ).decodeToString()
 
